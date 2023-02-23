225fff91f383ec4dc305e21a359ab6f989cf275a
brunongoncalve <103283382+brunongoncalve@users.noreply.github.com>
2023-02-23 16:42:43 -0300

    update 23.02.2023

--- RECONECT/app/Models/Integra/Post.php
+++ RECONECT/app/Models/Integra/Post.php
@@ -19,4 +19,9 @@ class Post extends Model
     {
         return $this->belongsTo('App\Models\Integra\Tag', 'rep002s_id', 'id');
     }
+
+    public function likePost()
+    {
+        return $this->belongsTo('App\Models\Integra\Like', 'rep001s_id', 'id');
+    }
 }
