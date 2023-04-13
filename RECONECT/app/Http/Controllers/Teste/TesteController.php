016eb2dae1e665c5bb218f1f9f950cbe6ef6ebaf
brunongoncalve <103283382+brunongoncalve@users.noreply.github.com>
2023-04-13 16:21:19 -0300

    update 13.04.2023

--- RECONECT/app/Http/Controllers/Teste/TesteController.php
+++ RECONECT/app/Http/Controllers/Teste/TesteController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Teste\Teste;
+use App\Exports\TarefasExport;
+use Maatwebsite\Excel\Facades\Excel;
 
 class TesteController extends Controller
 {
@@ -14,35 +16,8 @@ class TesteController extends Controller
         return view('Teste.teste_index');
     }
 
-    public function store(Request $request)
+    public function export(Request $request)
     {
-        if($request->hasFile('file')) {
-        } else {
-            $fileNameToStore = 'erroplanilha.xlsx';
-        }
-
-        $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
-            $reader->setReadDataOnly(true);
-            $planilha = $reader->load($request->file('file'));
-            $planilha->setActiveSheetIndexByName('Planilha1'); 
-            $coluna = $planilha->setActiveSheetIndex(0)->getHighestColumn();
-            $linha = $planilha->setActiveSheetIndex(0)->getHighestRow();
-            $celulas = $planilha->getActiveSheet()->toArray();
-            $celulas = array_slice($celulas, 0, 100);
-
-        DB::transaction(function() use ($request) {   
-            foreach($celulas as $li) {
-                $teste = new Teste;
-
-                $teste->field_1 = $li[0];
-                $teste->field_2 = $li[1];
-         
-                if($li[0]) 
-        
-               $teste->save();
-             }
-        });
-
-        return 'SUCESSO';
+        return Excel::download(new TarefasExport($request->bruno), 'tarefas.xlsx');
     }
 }
